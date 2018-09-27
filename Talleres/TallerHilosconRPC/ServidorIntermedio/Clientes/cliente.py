0fbaab061925c13e501a3d83278f5804220b86ff
Jofemago <jofemago@utp.edu.co>
2018-09-26 22:36:21 -0500

    .

---
+++ Talleres/TallerHilosconRPC/ServidorIntermedio/Clientes/cliente.py
@@ -0,0 +1,40 @@
+from SimpleXMLRPCServer import SimpleXMLRPCServer
+from SimpleXMLRPCServer import SimpleXMLRPCRequestHandler
+import SocketServer
+import SimpleXMLRPCServer
+import sys
+import threading
+import xmlrpclib
+import time
+from Scanner import Scanner
+
+
+
+class ClientThread(threading.Thread):
+    def __init__(self):
+    	self.sc=Scanner()
+    	threading.Thread.__init__(self)
+    	self.s = xmlrpclib.ServerProxy('http://localhost:9999')
+
+
+
+    def toString(self):
+    	x= "La operacion "+str(self.sc.getNro1())+" "+self.sc.getOperation()+" "+str(self.sc.getNro2())+" = "+str(mekeOperation(self.sc,self.s))
+    	return x
+
+    def run(self):
+		#time.sleep(3)
+        print "Llamada cliente1 "
+        print self.s.getServer(self.sc.getOperation())
+        #print self.toString()
+		#print  "La operacion ",self.sc.getNro1()," ",self.sc.getOperation()," ",self.sc.getNro2()," = ",mekeOperation(self.sc,self.s)
+		#print "Esto da: ",self.s.div(5,2)  # Returns 5//2 = 2
+		#print self.s.add(7,2)
+
+
+
+
+client = ClientThread()
+#client2 = ClientThread2()
+client.start() # The server is now running
+#client2.start()
---
+++ Talleres/TallerHilosconRPC/ServidorIntermedio/ServerNames.py
@@ -0,0 +1,33 @@
+from SimpleXMLRPCServer import SimpleXMLRPCServer
+from SimpleXMLRPCServer import SimpleXMLRPCRequestHandler
+import SocketServer
+import SimpleXMLRPCServer
+import sys
+import threading
+import xmlrpclib
+import time
+from Direccionador import *
+
+
+class SimpleThreadedXMLRPCServer(SocketServer.ThreadingMixIn, SimpleXMLRPCServer.SimpleXMLRPCServer):
+        pass
+
+class ServerThread(threading.Thread):
+    def __init__(self, puerto):
+         threading.Thread.__init__(self)
+         self.localServer = SimpleThreadedXMLRPCServer(("localhost",puerto))
+         #self.localServer.register_function(div)
+         self.localServer.register_instance(Direccionador())
+
+    def run(self):
+         self.localServer.serve_forever()
+
+
+
+
+
+server = ServerThread(9999)
+#server = ServerThread(9993)
+#server = ServerThread(9994)
+server.start() # The server is now running
+print "Listo servidor."
